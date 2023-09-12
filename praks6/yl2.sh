9bd140758cf08608c4bd767eba6c3f6ac447592f
Kevin Sutt <kevin.sutt@voco.ee>
2023-09-12 18:28:53 +0300

    lisatud uus fail

---
+++ praks6/yl2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Antud skript on taiendus vanast ulesandest
+# tapsemalt lisati juurde seda, et see korral, kui tekkivad reisijad, kes ei mahu taisbusside sisse, suurendab busside arv uhe vorra
+
+read -p "Sisestage mitu reisijat on grupis: " reisijad
+read -p "Sisestage kohtade arv bussis: " kohad # valitud arv on vaartusena 1 buss
+
+busse=$((reisijad / kohad)) # jagame inimesed busside vahel ara
+if [ $((reisijad % kohad)) -eq 0 ]; then # % aitab jaagi ules leida
+    echo "Kohad on täidetud!"
+else
+    busse=$((busse + 1))
+fi
+
+echo "Vaja on nii palju busse: $busse "
